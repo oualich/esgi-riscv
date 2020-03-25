9cec5b4e2ac00983600a55d9fe3f225a00389c13
oualich <ouali.cherikh@gmail.com>
2020-03-25 10:13:14 +0100

    add response 2+3

--- exercices/01_syscall.asm
+++ exercices/01_syscall.asm
@@ -11,10 +11,8 @@ message: .asciz "Hello World form .data"
 # - Dans quelle segment de la mémoire est elle?
 # .data
 # - Quelle sont les valeurs en hexadecimales présente dans .data?
-# 0x10010000 value : 0x6c6c6548, 0x6f57206f,0x20646c72,0x6d726f66,0x61642e20, 0x00006174
+# adress 0x10010000 value hexa : 0x00006174
 
 la a0, message
 li a7, 4 # appel systeme 4 selon la doc de Rars printString
 ecall
-
-# Bonus: avoir 64 dans un registre en une seule instruction
