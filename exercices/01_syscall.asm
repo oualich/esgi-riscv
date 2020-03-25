2978a467a57ec2a14645d452ebd3656c9ecfcd46
oualich <ouali.cherikh@gmail.com>
2020-03-25 14:51:55 +0100

    reponse 1 2 3

--- exercices/01_syscall.asm
+++ exercices/01_syscall.asm
@@ -7,11 +7,11 @@ message: .asciz "Hello World form .data"
 
 # Exercice: Observation dans Rars
 # - Quelle est l'addresse de base de la chaine msg? Son addresse de fin?
-# l'adresse de base est 0x10010000, et sont adresse de fin est ...
+# BASE : 0x1001_0000 . FIN : 0x1001_0014
 # - Dans quelle segment de la mémoire est elle?
 # .data
 # - Quelle sont les valeurs en hexadecimales présente dans .data?
-# adress 0x10010000 value hexa : 0x00006174
+# La chaine de charactère et son 0 à la fin
 
 la a0, message
 li a7, 4 # appel systeme 4 selon la doc de Rars printString
